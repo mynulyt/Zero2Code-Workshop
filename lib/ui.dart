abcbbc6b6a43aa451761896f3e938383b21339da
Mynul Alam <mynulalam27@gmail.com>
2025-07-25 21:07:00 +0600

    update

--- lib/ui.dart
+++ lib/ui.dart
@@ -6,35 +6,31 @@ class UiPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: 500, // Enough height to avoid cutoff
+      body: Container(
+        height: 600,
         child: Stack(
-          clipBehavior: Clip.none, // Allow overflow
           children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
+            Positioned(
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
                 ),
               ),
             ),
             Positioned(
               top: 250,
-              left: MediaQuery.of(context).size.width / 2 - 60, // Centered
+              left: 180,
               child: CircleAvatar(
                 radius: 60,
-                backgroundColor: Colors.white,
-                child: ClipOval(
-                  child: Image.network(
-                    "https://tse3.mm.bing.net/th/id/OIP.EwG6x9w6RngqsKrPJYxULAHaHa?cb=thvnextc2&rs=1&pid=ImgDetMain&o=7&rm=3",
-                    width: 110,
-                    height: 110,
-                    fit: BoxFit.cover,
-                  ),
+                child: Image.network(
+                  "https://tse3.mm.bing.net/th/id/OIP.EwG6x9w6RngqsKrPJYxULAHaHa?cb=thvnextc2&rs=1&pid=ImgDetMain&o=7&rm=3",
+                  fit: BoxFit.contain,
                 ),
               ),
             ),
